7d2c4d7022e2a97bfb6cfc26b8a5fa0bd9771d97
maornetzer9 <maorgithub@gmail.com>
2024-04-06 21:01:37 +0300

    # Update Proxy on package json

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -18,7 +18,7 @@ export default function ContactUs({ scrollToTop = () => {} }) {
     {
         try
         {
-            const request = "/email";
+            const request = "http://localhost:5050/email";
             const emailRegex = /\S+@\S+\.\S+/;
             const { message: userMessage, from, email } = contact;
 
